144642f98b270a9af354523580dfa95c351c731e
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-08-19 23:26:54 -0400

    Add 'delete(value)' function (iterative)

--- index.js
+++ index.js
@@ -39,6 +39,98 @@ class Tree {
 	};
 
 	/**
+	 * Delete `value` from tree
+	 * @param {number | any} value - Value to delete
+	 */
+	delete = (value) => {
+		let node = this.root;
+		let previous = this.root;
+		// If value is Root
+		if (value === node.value) {
+			node = node.right;
+			while (node.left != null) {
+				previous = node;
+				node = node.left;
+			}
+
+			if (previous !== this.root) {
+				previous.left = node.right;
+			}
+
+			node.right = this.root.right;
+			node.left = this.root.left;
+			this.root = node;
+
+			return;
+		}
+
+		while (true) {
+			// If value is greater than Node
+			if (value > node.value) {
+				// Loop to next Node (right)
+				previous = node;
+				node = node.right;
+			// If value is less than Node
+			} else if (value < node.value) {
+				// Loop to next Node (left)
+				previous = node;
+				node = node.left;
+			// If value = Node
+			} else if (value == node.value) {
+				/* If prev is greater than Node AND Node has children,
+				Means Nodes Right values (greater) go to
+				Prev's left (less), and Node's left values (less than node)
+				go to Prev's *new* left's left (less than new left) */
+				// eslint-disable-next-line max-len
+				if (previous.value > node.value && (node.right != null || node.left != null)) {
+					previous.left = node.right;
+					previous.left.left = node.left;
+				/* If Prev is less than Node AND Node has children,
+				Means Node's right values (greater) go to
+				Prev's right (greater), and Node's left values (less than Node)
+				go to Prev's *new* right's left (less than new right) */
+				// eslint-disable-next-line max-len
+				} else if (previous.value < node.value && (node.right != null || node.left != null)) {
+					previous.right = node.right;
+					previous.right.left = node.left;
+				/* Else, Node doesn't have children,
+				For some reason doesn't work if Node's children = `null` so
+				I have to check and do it differently */
+				} else {
+					/* If Prev is less than Node AND both Node's children are null,
+					Set Prev's right to null (delete Node) */
+					if (previous.value < node.value &&
+					node.right == null && node.left == null) {
+						previous.right = null; // Delete `Node`
+					/* If Prev is *greater* than Node AND both Node's children are null
+					Set Prev's left to null (delete Node) */
+					} else if (previous.value > node.value &&
+					node.right == null && node.left == null) {
+						previous.left = null; // Delete `Node`
+					/* If Prev is *greater* than Node AND Right child is null */
+					} else if (previous.value > node.value &&
+					node.right == null && node.left != null) {
+						previous.left = node.left;
+					/* If Prev is *greater* than Node AND Left child is null */
+					} else if (previous.value > node.value &&
+					node.right != null && node.left == null) {
+						previous.left = node.right;
+					/* If Prev is *less* than Node AND Right child is null */
+					} else if (previous.value < node.value &&
+					node.right == null && node.left != null) {
+						previous.right = node.left;
+					/* If Prev is *less* than Node AND Left child is null */
+					} else if (previous.value < node.value &&
+					node.right != null && node.left == null) {
+						previous.right = node.right;
+					}
+				}
+				break;
+			}
+		}
+	};
+
+	/**
 	 * Check if Tree contains value
 	 * @param {number | any} value - Value to check
 	 * @param {Node} check - Node to check (leave blank to run through whole tree)
@@ -135,5 +227,5 @@ const sorted = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
 	16, 17, 18, 19, 20];
 
 const tree = new Tree(sorted);
-tree.insert(21);
+tree.delete(10);
 tree.prettyPrint();
